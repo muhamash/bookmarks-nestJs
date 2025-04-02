526d45e05d2f77a2d141004405d3c9c863bc6948
muhamsh <muhammad-ashraful@outlook.com>
2025-04-03 00:29:01 +0600

    dto validation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,13 @@
-export interface AuthDto {
+import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
-  password: string;
+
+  // Allow both strings and numbers as passwords
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9]+$/, {
+    message: 'Password must be a string or a number',
+  }) // Alphanumeric check
+  password: string | number;
 }
