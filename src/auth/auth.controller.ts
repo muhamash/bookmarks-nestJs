95b1503d463827f5647bb8ab0442c768c1358dd0
muhamsh <muhammad-ashraful@outlook.com>
2025-04-03 00:01:54 +0600

    working on dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,12 +9,19 @@ export class AuthController {
   }
 
   @Post('signup')
-  signup() {
-    // return 'i am signUp';
-    // return {
-    //   message: 'signup route!!',
-    //   status: 'ok',
-    // };
+  // signup(@Req() req: Request) {
+  //   // return 'i am signUp';
+  //   // return {
+  //   //   message: 'signup route!!',
+  //   //   status: 'ok',
+  //   // };
+  //   console.log(req.body);
+  //   return this.authService.signup(req.body);
+  // }
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      dto,
+    });
     return this.authService.signup();
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   //   test() {
   //     return 'this is a test function in auth service';
   //   }
+
+  constructor(private prisma: PrismaService) {}
 
   signin() {
     return {
@@ -14,6 +17,10 @@ export class AuthService {
   }
 
   signup() {
-    return 'i am signIn';
+    // return 'i am signup responding the text';
+    return {
+      message: 'sign up responding',
+      status: 'ok',
+    };
   }
 }
