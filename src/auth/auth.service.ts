2adb8eb3b464a2c7ec9e8f791e476037dcf2d88e
muhamsh <muhammad-ashraful@outlook.com>
2025-04-03 12:50:54 +0600

    email duplication forbidden into database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
@@ -21,16 +22,35 @@ export class AuthService {
   async signup(dto: AuthDto) {
     // return 'i am signup responding the text';
     // generate the has password then save to the db
-    const hashPass = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hashPass,
-      },
-    });
+    try {
+      const hashPass = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hashPass,
+        },
+      });
 
-    console.log('user created', user);
-    return user;
+      // delete user.hash;
+
+      // console.log('user created', user);
+      // return user;
+      const { hash, ...userWithoutHash } = user;
+
+      console.log('User created:', userWithoutHash);
+      return userWithoutHash;
+    } catch (error) {
+      console.log(error);
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'this credential already regiostered into database',
+          );
+        }
+      }
+
+      throw error;
+    }
     // return {
     //   message: 'sign up responding',
     //   status: 'ok',
