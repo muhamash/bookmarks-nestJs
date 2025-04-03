9d79d911f790d563cde064e89a6982f3c9b24e67
muhamsh <muhammad-ashraful@outlook.com>
2025-04-03 13:10:25 +0600

    swagger api docs added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookmark api')
+    .setDescription('The test ->> bookmark API description')
+    .setVersion('1.0')
+    .addTag('bookmark')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   // Enable global validation
   app.useGlobalPipes(
